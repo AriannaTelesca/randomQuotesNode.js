e7d0f0086574085febd616c2d6b4b09db92d5c59
Arianna Telesca <arianna.tlsc@gmail.com>
2022-09-15 11:35:31 +0200

    test5

--- server/models/model.js
+++ server/models/model.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
 require('dotenv').config();
+const mongoose = require("mongoose");
 const apiQuotes = require("../api/functionQuotes");
 const mongoDB = process.env.DBurl;
 
