157a271eebf7b3dbd10d47b849f609b67fdc0105
Arianna Telesca <arianna.tlsc@gmail.com>
2022-09-15 10:59:51 +0200

    test4

--- server/routes/exe.routes.js
+++ server/routes/exe.routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiQuotes = require('../api/functionQuotes');
-let Model = require('../models/model');
+let Quotes = require('../models/model');
 
 
 //post
@@ -9,7 +9,7 @@ router.post('/post',(req, res) => {
     const author = req.body.author;
     const quote = req.body.quote;
 
-    const newQuote = new Model ({
+    const newQuote = new Quotes ({
         author,
         quote,
     });
@@ -22,14 +22,14 @@ router.post('/post',(req, res) => {
 
 //GET
 router.get("/getAll",(req, res) =>{
-    Model.find()
+    Quotes.find()
     .then(result => res.json(result))
     .catch(err => res.status(400).json('Error'+err))
 });
 
 //Get by Author
 router.get("/:author",(req, res,)=>{
-    Model.find({author: req.params.author})
+    Quotes.find({author: req.params.author})
     .then(result => res.json(result))
     .catch(err => res.status(400).json('Error'+err))
 });
